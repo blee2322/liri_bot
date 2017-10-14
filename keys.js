ba75171cdc85233e958bd8f0a9ff97c58287dac3
unknown <brandon.geeman.lee@gmail.com>
2017-10-14 09:23:53 -0700

    Adding Twitter Keys and NPM Installs

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'NHRI4gE4AaHuqiPG2xJWBwkWZ',
+  consumer_secret: 'LYTAyJVBioIxGe03OdLPmV7aLrzQmNgaCfIC1Qnq5qrVj3rFpW',
+  access_token_key: ' 2997185076-JV7w0PEKB033UXcxWm6RetFrS6NFK71awty9pId',
+  access_token_secret: 'bqi5pdOkRGjiYD6yLV0fIHWMoQcjYCJSrinp9AOSqe2u9',
+}
+
+module.exports = twitterKeys;
