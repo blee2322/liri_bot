f7a5098c7800d6991f039fb11221f08d6e849411
unknown <brandon.geeman.lee@gmail.com>
2017-10-14 19:17:58 -0700

    Spotify Working

--- liri.js
+++ liri.js
@@ -3,19 +3,20 @@ var fs = require("fs");
 var keys = require("./keys.js");
 // console.log(keys);
 var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
 
 var app = process.argv[2];
 var value = process.argv[3];
 
 switch(app) {
 
-  case "my-tweets":
-  tweet();
-  break;
-
-  // case "spotify-this-song":
-  // spot();
+  // case "my-tweets":
+  // tweet();
   // break;
+
+  case "spotify-this-song":
+  spot();
+  break;
 
   // case "movie-this":
   // mov();
@@ -28,59 +29,43 @@ switch(app) {
 
 //Twitter
 
-function tweet() {
+// function tweet() {
 
-    var client = new Twitter({
-      consumer_key: 'NDrNnJBlpm7JmT2OgUu26iTI8',
-      consumer_secret: 'h266D7nolgyQlTrni2Axvj8cf6GMNLhdP5iB2Sh7KmqR4yc5DI',
-      access_token_key: '2997185076-JV7w0PEKB033UXcxWm6RetFrS6NFK71awty9pId',
-      access_token_secret: 'bqi5pdOkRGjiYD6yLV0fIHWMoQcjYCJSrinp9AOSqe2u9'
-    });
-    // console.log(client);
+//     var client = new Twitter({
+//       consumer_key: 'NDrNnJBlpm7JmT2OgUu26iTI8',
+//       consumer_secret: 'h266D7nolgyQlTrni2Axvj8cf6GMNLhdP5iB2Sh7KmqR4yc5DI',
+//       access_token_key: '2997185076-JV7w0PEKB033UXcxWm6RetFrS6NFK71awty9pId',
+//       access_token_secret: 'bqi5pdOkRGjiYD6yLV0fIHWMoQcjYCJSrinp9AOSqe2u9'
+//     });
+//     // console.log(client);
  
-    var params = {screen_name:'wi', count: 20};
-    client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
-      if (!error) {
-        console.log(tweets);
-      }else if(error)
-        console.log(error);
-    });
-}
+//     var params = {screen_name:'wi', count: 20};
+//     client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
+//       if (!error) {
+//         console.log(tweets);
+//       }else if(error)
+//         console.log(error);
+//     });
+// }
 
-//   var Twitter = require("twitter");
-//   var config = require("./keys.js").twitterKeys;
-//   var client = new Twitter(config)
-//   var params = {
-//     screen_name: 'CNN',
-//     count: 20
-//   };
-//   console.log(params);
-//   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-
-//     if (error) {
-//         return console.log(error);
-//       }
-//     console.log(tweets, response);
-//   })
-// };
 
 //Spotify
 
-// function spot() {
+function spot() {
 
-//   var spotify = require("node-spotify-api");
-//   var spotifyClient = new spotify({
-//     clientId: "d63d45b395e14ece9d8c40e16aec571e",
-//     clientSecret: "02feedee9fd94e589f9028cbbdd37fdf"
-//   });
-
-//   spotifyApi.searchTracks( value, function(err, data) {
-//     if (err) {
-//       console.log(err);
-//     }
-//   });
-//   console.log(value);
-// };
+  var songName = value; 
+  var spotify = new Spotify({
+    id: "d63d45b395e14ece9d8c40e16aec571e",
+    secret: "efd96ed6fa024911848ed0cabffd0f2b"
+  });
+   
+  spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  console.log(data.tracks.items[0]); 
+  });
+}
 
 // function mov() {
 
