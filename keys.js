42a1130b97fe1183360c7bd4a9759e4f53ce8fde
unknown <brandon.geeman.lee@gmail.com>
2017-10-14 17:28:51 -0700

    twitter is working

--- keys.js
+++ keys.js
@@ -2,8 +2,8 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: ' NHRI4gE4AaHuqiPG2xJWBwkWZ',
-  consumer_secret: 'LYTAyJVBioIxGe03OdLPmV7aLrzQmNgaCfIC1Qnq5qrVj3rFpW',
+  consumer_key: 'NDrNnJBlpm7JmT2OgUu26iTI8',
+  consumer_secret: 'h266D7nolgyQlTrni2Axvj8cf6GMNLhdP5iB2Sh7KmqR4yc5DI',
   access_token_key: '2997185076-JV7w0PEKB033UXcxWm6RetFrS6NFK71awty9pId',
   access_token_secret: 'bqi5pdOkRGjiYD6yLV0fIHWMoQcjYCJSrinp9AOSqe2u9'
 }
--- liri.js
+++ liri.js
@@ -1,11 +1,11 @@
-var Twitter = require("twitter");
-var spotify = require("node-spotify-api");
-var request = require("request");
+
 var fs = require("fs");
-var config = require("./keys.js");
-console.log(config);
+var keys = require("./keys.js");
+// console.log(keys);
+var Twitter = require("twitter");
 
 var app = process.argv[2];
+var value = process.argv[3];
 
 switch(app) {
 
@@ -13,51 +13,87 @@ switch(app) {
   tweet();
   break;
 
-  case "spotify-this-song":
-  spot();
-  break;
+  // case "spotify-this-song":
+  // spot();
+  // break;
 
-  case "movie-this":
-  mov();
-  break;
+  // case "movie-this":
+  // mov();
+  // break;
 
-  case "do-what-it-says":
-  dowhat();
-  break;
+  // case "do-what-it-says":
+  // dowhat();
+  // break;
 }
 
 //Twitter
 
 function tweet() {
 
-  var client = new Twitter(config)
-  var params = {
-    screen_name: 'CNN',
-    count: 20
-};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    var client = new Twitter({
+      consumer_key: 'NDrNnJBlpm7JmT2OgUu26iTI8',
+      consumer_secret: 'h266D7nolgyQlTrni2Axvj8cf6GMNLhdP5iB2Sh7KmqR4yc5DI',
+      access_token_key: '2997185076-JV7w0PEKB033UXcxWm6RetFrS6NFK71awty9pId',
+      access_token_secret: 'bqi5pdOkRGjiYD6yLV0fIHWMoQcjYCJSrinp9AOSqe2u9'
+    });
+    // console.log(client);
+ 
+    var params = {screen_name:'wi', count: 20};
+    client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
+      if (!error) {
+        console.log(tweets);
+      }else if(error)
+        console.log(error);
+    });
+}
 
-    if (error) {
-        return console.log(error);
-      }
-    console.log(tweets);
-  })
-};
+//   var Twitter = require("twitter");
+//   var config = require("./keys.js").twitterKeys;
+//   var client = new Twitter(config)
+//   var params = {
+//     screen_name: 'CNN',
+//     count: 20
+//   };
+//   console.log(params);
+//   client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+//     if (error) {
+//         return console.log(error);
+//       }
+//     console.log(tweets, response);
+//   })
+// };
 
 //Spotify
 
 // function spot() {
 
-//   var spotifyClient = new SpotifyWebApi({
-//     clientId: "d63d45b395e14ece9d8c40e16aec571e"
+//   var spotify = require("node-spotify-api");
+//   var spotifyClient = new spotify({
+//     clientId: "d63d45b395e14ece9d8c40e16aec571e",
 //     clientSecret: "02feedee9fd94e589f9028cbbdd37fdf"
-//   })
-  
-//   spotifyApi.searchTracks(app)
-  
-//   .then(function(data) {
-//     console.log(data.body);
-//   }, function(err) {
-//     console.error(err);
 //   });
+
+//   spotifyApi.searchTracks( value, function(err, data) {
+//     if (err) {
+//       console.log(err);
+//     }
+//   });
+//   console.log(value);
+// };
+
+// function mov() {
+
+//   var request = require("request");
+//   var movieTitle = value;
+
+//   request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+
+//   // If the request is successful (i.e. if the response status code is 200)
+//   if (!error && response.statusCode === 200) {
+
+//     // Parse the body of the site and recover just the imdbRating
+//     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+//     console.log(body);
+//   }
 // }
